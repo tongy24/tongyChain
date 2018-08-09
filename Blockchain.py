35dc5be852be373cc7bca0f687fef8bf64c871e8
Harry Tong <harry@tongy.io>
2018-08-10 00:22:17 +0100

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -11,7 +11,7 @@ import json
 from hashlib import sha256
 import time
 import datetime
-
+import os
 # # Creating a single block
 
 # In[2]:
@@ -128,7 +128,7 @@ from flask import render_template, redirect
 import requests
 
 app = Flask(__name__)
-CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
+CONNECTED_NODE_ADDRESS = str(os.curdir)
 blockchain = Blockchain()
 peers = set()
 
