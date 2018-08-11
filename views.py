e7327b08ff575938eccd24d6b3356d22e5d7d9a4
Harry Tong <harry@tongy.io>
2018-08-11 02:20:24 +0100

    Update views.py

--- views.py
+++ views.py
@@ -8,7 +8,7 @@ from app import app
 
 # The node with which our application interacts, there can be multiple
 # such nodes as well.
-CONNECTED_NODE_ADDRESS = str(os.curdir)
+CONNECTED_NODE_ADDRESS =  "http://127.0.0.1:8000"
 
 posts = []
 
